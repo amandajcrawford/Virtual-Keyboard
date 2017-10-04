1ba96561c7f141c60704c66f8641c07271d01633
Amanda Crawford <amc315.pitt.edu>
2017-10-04 04:03:52 -0400

    --Test website infra

---
+++ website.js
@@ -0,0 +1,8 @@
+var express = require(‘express’);
+var app = express();
+app.get(‘/’, function (req, res) {
+res.send(‘<h1>Open Source For You!</h1>’);
+});
+app.listen(3000, function () {
+    console.log(‘Example app listening on port 3000!’);
+});
